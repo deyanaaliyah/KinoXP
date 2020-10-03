a7452316991c9718bda468fcdccc88e882e0cd95
Mugisha98 <58075884+Mugisha98@users.noreply.github.com>
2020-10-03 13:28:16 +0200

    Controller class

--- src/main/java/mikroblod/kinoxp/demo/controller/MovieController.java
+++ src/main/java/mikroblod/kinoxp/demo/controller/MovieController.java
@@ -1,5 +1,6 @@
 package mikroblod.kinoxp.demo.controller;
 
+import mikroblod.kinoxp.demo.repository.IMovieRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MovieController {
 
-    IMovie movieRepository;
+    IMovieRepository movieRepository;
 
-    public MovieController(IMovie movieRepository){
+    public MovieController(IMovieRepository movieRepository){
         this.movieRepository = movieRepository;
     }
 
