1fdd97fd6ac12336d2c7b00809ee3094bb621b67
moha6248 <51365122+moha6248@users.noreply.github.com>
2020-10-07 16:34:54 +0200

    employeeHtml

--- src/main/java/mikroblod/kinoxp/demo/controller/EmployeeController.java
+++ src/main/java/mikroblod/kinoxp/demo/controller/EmployeeController.java
@@ -1,13 +1,21 @@
 package mikroblod.kinoxp.demo.controller;
 
+import mikroblod.kinoxp.demo.repository.EmployeeDBRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
+EmployeeDBRepository employeeRepository;
+
+    public EmployeeController(EmployeeDBRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
     @GetMapping("/roster")
-    public String menu(){
+    public String menu(Model model){
+        model.addAttribute("rosterOverview", employeeRepository.readAll());
         return "/roster";
     }
 
